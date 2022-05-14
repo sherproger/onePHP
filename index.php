a2caaa92cf57823eeeb06496a8f84c045cf043a9
sherproger <sherzad_90@mail.ru>
2022-05-14 19:36:30 +0600

    my project

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>JS History</title>
+	    <style>
+      body{
+        background-color:grey;
+        color:white;
+      }
+    </style>
+</head>
+<body>
+	<a href="first_page.php">page1</a>
+	<a href="second_page.php">page2</a>
+	<a href="third_page.php">page3</a>
+	
+</body>
+</html>
